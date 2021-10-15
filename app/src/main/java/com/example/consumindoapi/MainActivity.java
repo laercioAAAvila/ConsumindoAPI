52aa4e7ca38d7a6d7da64aa8893e231b0cbf81ed
naatsuh <ks.full.ks@gmail.com>
2021-10-14 22:26:32 -0300

    data filter json *is not done*

--- app/src/main/java/com/example/consumindoapi/MainActivity.java
+++ app/src/main/java/com/example/consumindoapi/MainActivity.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonFind;
-    private TextView textViewCep, textViewStreet, textViewCity, textViewState;
+    private TextView textViewCep, textViewDistrict , textViewStreet, textViewCity, textViewState;
     private EditText editTextCep;
 
     @Override
@@ -24,13 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         buttonFind = (Button) findViewById(R.id.button);
         textViewCep = (TextView) findViewById(R.id.textViewCEP);
+        textViewDistrict = (TextView) findViewById(R.id.textViewDistrict);
+        textViewStreet = (TextView) findViewById(R.id.textViewStreet);
+        textViewCity = (TextView) findViewById(R.id.textViewCity);
+        textViewState = (TextView) findViewById(R.id.textViewState);
         editTextCep = (EditText) findViewById(R.id.inputCEP);
+
         buttonFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String getCep;
                 getCep = editTextCep.getText().toString();
-
 
                 Tarefa tarefa = new Tarefa();
                 tarefa.execute("https://viacep.com.br/ws/"+getCep+"/json/");
@@ -49,32 +53,54 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             try {
-                String string1 = s.toString();
-                string1 = string1.replace("{","");
-                string1 = string1.replace("}","");
-                string1 = string1.replace("-","");
-                string1 = string1.replace("\"","");
-                string1 = string1.replace(",","");
-                String strings2[] = string1.split(":");
-                endereco(strings2);
-                /*for(int i = 0;i < strings2.length;i++) {
-                    System.out.print(strings2[i]);
-                }*/
-
+                endereco(s);
             }catch (Exception e){
                 e.printStackTrace();
             }
             textViewCep.setText(s);
         }
 
-        private String[] endereco (String[] s) {
-            String antigoEndereco[] = s;
-            for (int i = 0; i < antigoEndereco.length; i++){
-                if (antigoEndereco[i] == "cep"){
-                    antigoEndereco[i] = "Sem Informação";
-                }if (antigoEndereco[i] == "");
+        private String[] endereco (String s) {
+            String reformula = s.toString();
+            reformula = reformula.replace("{","");
+            reformula = reformula.replace("}","");
+            reformula = reformula.replace("-","");
+            reformula = reformula.replace("\"","");
+            reformula = reformula.replace(" ", "");
+            reformula = reformula.replace(":,", ": Sem informação,");
+            reformula = reformula.replace(":", ": ");
+            //======================================================
+            String vetorString[], cep, logradouro, bairro, localidade, uf, endereco[] = new String[5];
+
+            vetorString = reformula.split(",");
+
+            cep = vetorString[0];
+            logradouro = vetorString[1];
+            bairro = vetorString[3];
+            localidade = vetorString[4];
+            uf = vetorString[5];
+
+            endereco[0]= cep.replace("cep: ", "");
+            endereco[1]= logradouro.replace("logradouro: ", "");
+            endereco[2]= bairro.replace("bairro: ", "");
+            endereco[3]= localidade.replace("localidade: ", "");
+            endereco[4]= uf.replace("uf: ", "");
+
+            /*
+            endereco[0]= cep;
+            endereco[1]= logradouro;
+            endereco[2]= bairro;
+            endereco[3]= localidade;
+            endereco[4]= uf;*/
+
+            //=======================================================
+            /*
+            for (int i = 0; i < endereco.length; i++) {
+                System.out.println(endereco[i]);
             }
-            return antigoEndereco;
+            */
+            //=======================================================
+            return endereco;
         }
     }
 }
